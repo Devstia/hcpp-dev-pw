b8938af98effc5a848b2e0009918e33f1e2e9940
Stephen J. Carnam <steve@steveorevo.com>
2023-07-01 22:23:04 -0700

    wip

--- cg-pws.php
+++ cg-pws.php
@@ -1,2 +1,4 @@
 <?php
 /**
+// openssl  genrsa -out ./pws.key 2048 2>&1
+// openssl req -x509 -new -nodes -key ./pws.key -sha256 -days 825 -out ./pws.crt -subj "/C=US/ST=California/L=San Diego/O=CodeGarden PWS/OU=Customers/CN=pws.localhost" 2>&1
