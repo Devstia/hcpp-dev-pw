5abd2c4be6c77f32e97dab99cc8e90e1ab2ac69d
Stephen J. Carnam <steve@steveorevo.com>
2023-07-02 10:46:44 -0700

    wip

--- cg-pws.php
+++ cg-pws.php
@@ -1,4 +1,25 @@
 <?php
 /**
+ * Extend the HestiaCP Pluginable object with our CG_PWS object for
+ * providing a localhost development server functionality.
+ * 
+ * @version 1.0.0
+ * @license GPL-3.0
+ * @link https://github.com/virtuosoft-dev/hcpp-cg-pws
+ * 
+ */
+
+if ( ! class_exists( 'CG_PWS') ) {
+    class CG_PWS {
+        
+        /**
+         * Constructor, listen for add, update, or remove users
+         */
+        public function __construct() {
+        }
+    }
+    new CG_PWS(); 
+} 
+/**
 // openssl  genrsa -out ./pws.key 2048 2>&1
 // openssl req -x509 -new -nodes -key ./pws.key -sha256 -days 825 -out ./pws.crt -subj "/C=US/ST=California/L=San Diego/O=CodeGarden PWS/OU=Customers/CN=pws.localhost" 2>&1
--- plugin.php
+++ plugin.php
@@ -1,3 +1,16 @@
 <?php
 /**
- 
+ * Plugin Name: CG-PWS
+ * Plugin URI: https://github.com/virtuosoft-dev/hcpp-cg-pws
+ * Description: Code Garden Personal Web Server plugin for HestiaCP enables Code Garden features on the desktop app.
+ * Version: 1.0.0
+ * Author: Stephen J. Carnam
+ *
+ */
+
+// Register the install and uninstall scripts
+global $hcpp;
+require_once( dirname(__FILE__) . '/cg-pws.php' );
+
+$hcpp->register_install_script( dirname(__FILE__) . '/install' );
+$hcpp->register_uninstall_script( dirname(__FILE__) . '/uninstall' );
