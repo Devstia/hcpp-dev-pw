f7bec92b07767a0410444268460415b8305a9748
Steveorevo <steve@steveorevo.com>
2023-07-06 10:29:32 -0700

    wip

--- cg-pws.php
+++ cg-pws.php
@@ -13,13 +13,14 @@ if ( ! class_exists( 'CG_PWS') ) {
     class CG_PWS {
         
         /**
-         * Constructor, listen for add, update, or remove users
+         * Constructor, listen for adding, or listing websites
          */
         public function __construct() {
+            global $hcpp;
+            $hcpp->collabora = $this;
         }
     }
     new CG_PWS(); 
 } 
-/**
 // openssl  genrsa -out ./pws.key 2048 2>&1
 // openssl req -x509 -new -nodes -key ./pws.key -sha256 -days 825 -out ./pws.crt -subj "/C=US/ST=California/L=San Diego/O=CodeGarden PWS/OU=Customers/CN=pws.localhost" 2>&1
