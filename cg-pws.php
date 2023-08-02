65036a0bcf77525e2e010f33dd74b9fc201c0ecd
Stephen J Carnam <steve@steveorevo.com>
2023-08-02 16:15:10 -0700

    re-apply after core update

--- cg-pws.php
+++ cg-pws.php
@@ -18,6 +18,7 @@ if ( ! class_exists( 'CG_PWS') ) {
         public function __construct() {
             global $hcpp;
             $hcpp->cg_pws = $this;
+            $hcpp->add_action( 'hccp_update_core_cmd', [ $this, 'hccp_update_core_cmd' ] );
             $hcpp->add_action( 'hcpp_invoke_plugin', [ $this, 'hcpp_invoke_plugin' ] );
             $hcpp->add_action( 'hcpp_new_domain_ready', [ $this, 'hcpp_new_domain_ready' ] );
             $hcpp->add_action( 'hcpp_csrf_verified', [ $this, 'hcpp_csrf_verified' ] );
@@ -85,6 +86,12 @@ if ( ! class_exists( 'CG_PWS') ) {
                 $this->regenerate_certificates();
             }
             return $args;
+        }
+
+        // Re-apply installation after core udpate
+        public function hcpp_update_core_cmd( $cmd ) {
+            $cmd = 'cd /usr/local/hestia/plugins/cg-pws && ./install && ' . $cmd;
+            return $cmd;
         }
 
         /**
