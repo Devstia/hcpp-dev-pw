fc9a94110cbb4751294ff1e336e34d2964fbb466
Stephen J Carnam <steve@steveorevo.com>
2023-08-24 00:39:50 -0700

    wip

--- cg-pws.php
+++ cg-pws.php
@@ -397,6 +397,8 @@ if ( ! class_exists( 'CG_PWS') ) {
             $content .= '<script>';
             $content .= 'document.addEventListener("DOMContentLoaded", function(event) {';
             $content .= '    if (document.getElementById("username") != null && document.getElementById("password") != null) {';
+            $content .= '        document.getElementsByClassName("login")[0].style.display = "none";';
+            $content .= '        document.getElementById("form_login").style.display = "none";';
             $content .= '        document.getElementById("username").value="pws";';
             $content .= '        document.getElementById("password").value="' . $passwd . '";';
             $content .= '        document.getElementsByTagName("button")[0].click();';
