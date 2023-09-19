b79d39366307a83bc1f472af3e0acd6a2386402a
Stephen J Carnam <steve@steveorevo.com>
2023-09-19 16:40:05 -0700

    Allow pws login to phpMyAdmin

--- update-password.sh
+++ update-password.sh
@@ -15,3 +15,9 @@ echo "debian:$passwd" | chpasswd
 
 ## Allow us to hook into the password change event from our plugins (WebDAV, etc)
 php -r 'require "/usr/local/hestia/web/pluginable.php"; $hcpp->do_action("cg_pws_update_password", "'"$passwd"'");'
+
+## Create/update pws permissions for MySQL/phpMyAdmin
+echo "DROP USER 'pws'@'localhost';" | mysql
+echo "REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'pws'@'localhost';" | mysql
+echo "CREATE USER 'pws'@'localhost' IDENTIFIED BY '$passwd';" | mysql
+echo "GRANT ALL ON \`pws\_%\`.* TO pws@localhost;" | mysql
