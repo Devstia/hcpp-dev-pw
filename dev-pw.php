c28323014b56ab6eb2175b8b1d5b6318b20b66c6
Stephen J Carnam <steve@steveorevo.com>
2023-12-14 13:33:33 -0800

    get lanIP

--- dev-pw.php
+++ dev-pw.php
@@ -95,7 +95,7 @@ if ( ! class_exists( 'DEV_PW') ) {
                     )
                 );
                 $context = stream_context_create($options);
-                $hcpp->log( file_get_contents( 'http://10.0.2.2:8088/', false, $context) );
+                $lanIP = file_get_contents( 'http://10.0.2.2:8088/', false, $context );
             }catch( Exception $e ) {
                 $hcpp->log( 'Error in DEV_PW->publish_certs_keys: ' . $e->getMessage() );
             }
